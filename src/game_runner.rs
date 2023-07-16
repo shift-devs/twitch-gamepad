da2b82880f7346cbdc2026460a5b7a3788fb0a8c
antangelo <contact@antangelo.com>
2023-07-15 21:34:52 -0400

    Guarantee that running game, program are terminated on panic

--- src/game_runner.rs
+++ src/game_runner.rs
@@ -3,6 +3,9 @@ use nix::sys::signal::{kill, Signal};
 use tokio::process::Child;
 use tracing::info;
 
+use std::sync::atomic::{Ordering, AtomicI32};
+use std::sync::Arc;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum GameRunner {
@@ -21,7 +24,7 @@ async fn wait_on_child(child: &mut Option<Child>) -> anyhow::Result<()> {
     }
 }
 
-async fn stop_child(child: &mut Option<Child>) -> anyhow::Result<()> {
+async fn stop_child(child: &mut Option<Child>, child_pid_atomic: &Arc<AtomicI32>) -> anyhow::Result<()> {
     if let Some(child) = child {
         info!("Exiting current child");
         if let Some(pid) = child.id() {
@@ -34,29 +37,35 @@ async fn stop_child(child: &mut Option<Child>) -> anyhow::Result<()> {
             child.kill().await?;
         }
 
+        child_pid_atomic.store(0, Ordering::Relaxed);
         info!("Child should be gone now");
     }
 
     Ok(())
 }
 
-async fn game_runner_loop(mut rx: tokio::sync::mpsc::Receiver<GameRunner>) -> anyhow::Result<()> {
+async fn game_runner_loop(mut rx: tokio::sync::mpsc::Receiver<GameRunner>, child_pid_atomic: Arc<AtomicI32>) -> anyhow::Result<()> {
     let mut current_process: Option<tokio::process::Child> = None;
 
     loop {
         tokio::select! {
             cmd = rx.recv() => {
                 match cmd {
-                    Some(GameRunner::Stop) => stop_child(&mut current_process).await?,
+                    Some(GameRunner::Stop) => stop_child(&mut current_process, &child_pid_atomic).await?,
                     Some(GameRunner::SwitchTo(gc)) => {
-                        stop_child(&mut current_process).await?;
-                        current_process = Some(tokio::process::Command::new(gc.command)
+                        stop_child(&mut current_process, &child_pid_atomic).await?;
+                        let new_process = tokio::process::Command::new(gc.command)
                             .args(gc.args)
-                            .spawn()?)
+                            .spawn()?;
+                        child_pid_atomic.store(match new_process.id() {
+                            Some(pid) => pid as i32,
+                            None => 0,
+                        }, Ordering::Relaxed);
+                        current_process = Some(new_process);
                     }
                     _ => {
                         tracing::info!("Game runner done");
-                        stop_child(&mut current_process).await?;
+                        stop_child(&mut current_process, &child_pid_atomic).await?;
                         break Ok(());
                     },
                 }
@@ -75,8 +84,26 @@ pub fn run_game_runner() -> (
 ) {
     let (tx, rx) = tokio::sync::mpsc::channel(20);
 
+    let child_pid_atomic = Arc::new(AtomicI32::new(0));
+
+    {
+        let child_pid_atomic = child_pid_atomic.clone();
+        let panic_handler = std::panic::take_hook();
+        std::panic::set_hook(Box::new(move |panic_info| {
+            eprintln!("Panic handler invoked, killing child process");
+            let pid = child_pid_atomic.load(Ordering::Relaxed);
+            if pid != 0 {
+                eprintln!("Sending kill signal to {}", pid);
+                let pid = nix::unistd::Pid::from_raw(pid);
+                let _ignored = kill(pid, Signal::SIGTERM);
+            }
+
+            panic_handler(panic_info);
+        }));
+    }
+
     let handle = tokio::task::spawn(async move {
-        game_runner_loop(rx).await?;
+        game_runner_loop(rx, child_pid_atomic).await?;
         Ok(())
     });
 
